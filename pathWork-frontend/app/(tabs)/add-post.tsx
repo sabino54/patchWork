a98724cc4fb81ecd31c966bd180aceccedeace71
Avi Udash <udashavi@gmail.com>
2025-05-08 00:11:26 -0700

    various changes for post made

--- pathWork-frontend/app/(tabs)/add-post.tsx
+++ pathWork-frontend/app/(tabs)/add-post.tsx
@@ -1,12 +1,33 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
 
 export default function AddPost() {
-  const [selectedOption, setSelectedOption] = useState<
-    "photo" | "audio" | null
-  >(null);
+  const [selectedPostType, setSelectedPostType] = useState<"photo" | "audio">(
+    "photo",
+  );
+
+  const [tags, setTags] = useState<string[]>([]);
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handleAddTagButtonPress = () => {
+    setTags([...tags, "New Tag"]);
+  };
+
+  const handleTagDelete = (index: number) => {
+    const newTags = [...tags];
+    newTags.splice(index, 1);
+    setTags(newTags);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -14,45 +35,34 @@ export default function AddPost() {
         <Text style={styles.title}>Create New Post</Text>
       </View>
 
-      <View style={styles.optionsContainer}>
+      <View style={styles.postTypes}>
         <TouchableOpacity
           style={[
-            styles.optionButton,
-            selectedOption === "photo" && styles.selectedOption,
+            styles.postTypeButton,
+            selectedPostType === "photo" && styles.selectedPostType,
           ]}
-          onPress={() => setSelectedOption("photo")}
+          onPress={() => setSelectedPostType("photo")}
         >
-          <FontAwesome
-            name="camera"
-            size={40}
-            color={selectedOption === "photo" ? "#a084ca" : "#666"}
-          />
           <Text
             style={[
-              styles.optionText,
-              selectedOption === "photo" && styles.selectedText,
+              styles.postTypeText,
+              selectedPostType === "photo" && styles.selectedText,
             ]}
           >
             Photo
           </Text>
         </TouchableOpacity>
-
         <TouchableOpacity
           style={[
-            styles.optionButton,
-            selectedOption === "audio" && styles.selectedOption,
+            styles.postTypeButton,
+            selectedPostType === "audio" && styles.selectedPostType,
           ]}
-          onPress={() => setSelectedOption("audio")}
+          onPress={() => setSelectedPostType("audio")}
         >
-          <FontAwesome
-            name="microphone"
-            size={40}
-            color={selectedOption === "audio" ? "#a084ca" : "#666"}
-          />
           <Text
             style={[
-              styles.optionText,
-              selectedOption === "audio" && styles.selectedText,
+              styles.postTypeText,
+              selectedPostType === "audio" && styles.selectedText,
             ]}
           >
             Audio
@@ -60,7 +70,7 @@ export default function AddPost() {
         </TouchableOpacity>
       </View>
 
-      {selectedOption === "photo" && (
+      {selectedPostType === "photo" && (
         <View style={styles.uploadContainer}>
           <TouchableOpacity style={styles.uploadButton}>
             <FontAwesome name="cloud-upload" size={30} color="#a084ca" />
@@ -69,7 +79,7 @@ export default function AddPost() {
         </View>
       )}
 
-      {selectedOption === "audio" && (
+      {selectedPostType === "audio" && (
         <View style={styles.uploadContainer}>
           <TouchableOpacity style={styles.uploadButton}>
             <FontAwesome name="cloud-upload" size={30} color="#a084ca" />
@@ -77,6 +87,48 @@ export default function AddPost() {
           </TouchableOpacity>
         </View>
       )}
+
+      <View style={styles.tagsContainer}>
+        {tags.map((tag, index) => (
+          <View key={index} style={styles.tagButton}>
+            <Text style={styles.tagText}>{tag}</Text>
+            <TouchableOpacity onPress={() => handleTagDelete(index)}>
+              <FontAwesome name="times" size={16} color={"white"} />
+            </TouchableOpacity>
+          </View>
+        ))}
+
+        <TouchableOpacity
+          style={styles.addTagButton}
+          onPress={handleAddTagButtonPress}
+        >
+          <Text style={{}}>+ Add Tag</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.textContainer}>
+        <KeyboardAvoidingView
+          behavior={"padding"}
+          style={{ flex: 1 }}
+        >
+          <TextInput
+            placeholder="Enter a title"
+            value={title}
+            onChangeText={setTitle}
+            style={{ ...styles.titleInput, color: title ? "#000" : "#333" }}
+          />
+          <TextInput
+            placeholder="Enter description here..."
+            multiline
+            value={description}
+            onChangeText={setDescription}
+            style={{
+              ...styles.descriptionInput,
+              color: description ? "#000" : "#333",
+            }}
+          />
+        </KeyboardAvoidingView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -84,62 +136,105 @@ export default function AddPost() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f7f0fa",
+    gap: 20,
+    overflow: "scroll",
   },
+
+  // Header
   header: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: "#eee",
+    padding: 10,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     color: "#333",
+    textAlign: "center",
   },
-  optionsContainer: {
+
+  // Post Type Selection
+  postTypes: {
+    display: "flex",
     flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 20,
-    marginTop: 20,
+    width: "100%",
+    gap: 10,
+    justifyContent: "center",
   },
-  optionButton: {
+  postTypeButton: {
     alignItems: "center",
-    padding: 20,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
     borderRadius: 15,
-    backgroundColor: "#f5f5f5",
-    width: "45%",
+    borderWidth: 1,
+    borderColor: "#ddd",
   },
-  selectedOption: {
+  selectedPostType: {
     backgroundColor: "#f0e6ff",
-    borderWidth: 2,
-    borderColor: "#a084ca",
   },
-  optionText: {
-    marginTop: 10,
+  postTypeText: {
     fontSize: 16,
     color: "#666",
   },
   selectedText: {
     color: "#a084ca",
-    fontWeight: "bold",
   },
+
+  // Upload Section
   uploadContainer: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
   },
   uploadButton: {
     alignItems: "center",
+    justifyContent: "center",
     padding: 20,
-    borderRadius: 15,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#ddd",
     width: "100%",
+    height: 200,
   },
   uploadText: {
-    marginTop: 10,
     fontSize: 16,
     color: "#a084ca",
     fontWeight: "bold",
+  },
+
+  // Tags Section
+  tagsContainer: {
+    flexDirection: "row",
+    padding: 10,
+    gap: 10,
+  },
+  tagButton: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+    backgroundColor: "#a084ca",
+    borderRadius: 15,
+  },
+  tagText: {
+    color: "white",
+  },
+  addTagButton: {
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 15,
+    padding: 8,
+  },
+
+  // Text Section
+  textContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+  titleInput: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  descriptionInput: {
+    fontSize: 12,
   },
 });
